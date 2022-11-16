879078ead118a60f43c975db7f98f175a02284f0
Sam A <s.attwood@mmu.ac.uk>
2022-11-16 14:28:58 +0000

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Streamlit Starter')
+
+st.write('This repository was generated from the streamlit-starter template: .')
+
+st.write('To make this streamlit app your own, check out the streamlit documentation: .')
