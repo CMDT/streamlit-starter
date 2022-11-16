a1c7f62ff67e693c3a83bc833b5ce566089c1b66
Sam A <s.attwood@mmu.ac.uk>
2022-11-16 14:49:18 +0000

    Update app.py
    - add URLs

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title('Streamlit Starter')
 
-st.write('This repository was generated from the streamlit-starter template: .')
+st.write('This repository was generated from the streamlit-starter template: https://github.com/samattwood9/streamlit-starter.')
 
-st.write('To make this streamlit app your own, check out the streamlit documentation: .')
+st.write('To make this streamlit app your own, check out the streamlit documentation: https://docs.streamlit.io/.')
