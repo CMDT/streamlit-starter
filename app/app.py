0e46c050fd5929dc94fc68b6c32a5b4b9312c3a1
Sam A <s.attwood@mmu.ac.uk>
2022-11-16 15:34:00 +0000

    Reformat (via black)
    - Add deployment info to example page

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
-st.title('Streamlit Starter')
+st.title("Streamlit Starter")
 
-st.write('This repository was generated from the streamlit-starter template: https://github.com/samattwood9/streamlit-starter.')
+st.write(
+    "This repository was generated from the streamlit-starter template: https://github.com/samattwood9/streamlit-starter."
+)
 
-st.write('To make this streamlit app your own, check out the streamlit documentation: https://docs.streamlit.io/.')
+st.write(
+    "To make this streamlit app your own, check out the streamlit documentation: https://docs.streamlit.io/."
+)
+
+st.write(
+    "To deploy, check out streamlit cloud: https://docs.streamlit.io/streamlit-cloud."
+)
